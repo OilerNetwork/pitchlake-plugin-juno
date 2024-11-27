767333846d6887a705bfcaab203c0fac0fe8db4b
Dhruv Gupta <dhruv035@gmail.com>
2024-11-27 21:23:36 +0530

    Updates (#20)

--- db/migrations/000001_init_schema.down.sql
+++ db/migrations/000001_init_schema.down.sql
@@ -1,9 +1,9 @@
+DROP TABLE IF EXISTS public."Queued_Liquidity";
 DROP TABLE IF EXISTS public."VaultStates";
 DROP TABLE IF EXISTS public."Vault_Historic";
 DROP TABLE IF EXISTS public."Liquidity_Providers";
 DROP TABLE IF EXISTS public."Liquidity_Providers_Historic";
 DROP TABLE IF EXISTS public."Option_Rounds";
-DROP TABLE IF EXISTS public."Queued_Liquidity";
 DROP TABLE IF EXISTS public."Bids";
 DROP TABLE IF EXISTS public."Option_Buyers";
 
