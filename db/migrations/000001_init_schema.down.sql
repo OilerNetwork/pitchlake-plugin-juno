aeb1c38c1e5f38546a5485a01b540a83f475f8f7
Eugen Patru <eugen.ptr@gmail.com>
2024-12-06 10:59:25 +0700

    patching migration files

--- db/migrations/000001_init_schema.down.sql
+++ db/migrations/000001_init_schema.down.sql
@@ -6,4 +6,3 @@ DROP TABLE IF EXISTS public."Liquidity_Providers_Historic";
 DROP TABLE IF EXISTS public."Option_Rounds";
 DROP TABLE IF EXISTS public."Bids";
 DROP TABLE IF EXISTS public."Option_Buyers";
-
